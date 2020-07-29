7aefce6ecbd84d0e34705c239f0c9c9e77e21e0a
Nicolas <nikoalegre@hotmail.com>
2020-07-29 20:47:32 -0300

    Add config db

--- db.js
+++ db.js
@@ -1,10 +1,12 @@
 const { Sequelize } = require('sequelize');
 
 
-const db = new Sequelize('dblll', 'root', '', {
-  host: 'localhost',
+const db = new Sequelize('heroku_4bef9b172bccd57', 'bc656913221722', 'a4030ba3', {
+  host: 'us-cdbr-east-02.cleardb.com',
   dialect: 'mysql'
 });
+
+//bc656913221722:a4030ba3@us-cdbr-east-02.cleardb.com/heroku_4bef9b172bccd57
 
 module.exports = db;
 
