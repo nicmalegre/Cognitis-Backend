f738d28962ab7aacc74f6bfaceebfcf939307924
Sebastian-Rodriguez <sebasrz.rcia@gmail.com>
2020-08-13 01:15:55 -0300

    change port

--- index.js
+++ index.js
@@ -30,8 +30,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Running`);
+app.set('port',process.env.PORT || 3000)
+
+app.listen(app.get('port'), () => {
+  console.log(`Running ${ app.get('port')}`);
 });
 
 const transporter = nodemailer.createTransport(
