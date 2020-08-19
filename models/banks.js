2564d57435320f228ae40239e896913c758f4d0a
Sebastian-Rodriguez <sebasrz.rcia@gmail.com>
2020-08-19 02:41:21 -0300

    improving models

--- models/banks.js
+++ models/banks.js
@@ -1,25 +1,32 @@
-const { Sequelize, DataTypes } = require('sequelize');
-const db = require('../db');
+const { Sequelize, DataTypes } = require("sequelize");
+const db = require("../db");
 
-const banks = db.define('banks', {
+const banks = db.define(
+  "banks",
+  {
     bank_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
     },
     bank_name: {
-        type: DataTypes.STRING,
-        allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     bank_acount: {
-        type: DataTypes.STRING,
-        allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     bank_alias: {
-        type: DataTypes.STRING,
-        allowNull: false
-    }
-})
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: false,
+    freezeTableName: true,
+  }
+);
 
 module.exports = banks;
