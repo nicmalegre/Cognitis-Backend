e7af7c5a7d217c050bfe33d2bc735cc4cd097fc4
Nicolas <nikoalegre@hotmail.com>
2020-08-19 12:43:57 -0300

    Added the method for getusers, getuser, saveuser, emailverification

--- controllers/usercontrollers.js
+++ controllers/usercontrollers.js
@@ -1,11 +1,86 @@
 const userCrtl = {};
 const user = require('../models/user');
 
-//obtine todas las notas
+//GET ALL THE USERS
 userCrtl.getUsers= async(req,res) => {
     const users = await user.findAll(); //devuelve todos las notas
     res.json(users)
 }
+
+//FIND ONE USER WITH AN EMAIL
+userCrtl.getUser= async(req,res) => {
+    const newuser = await user.findOne({
+        where: {
+            user_mail: req.body.user_mail
+        }
+    })
+    if(newuser){
+        res.send(newuser)
+    }else
+    {
+        res.send(false)
+    }
+}
+
+//SAVE ONE NEW USER
+userCrtl.saveUser= async(req,res) => {
+    //Check if the email exists in the DB
+  user.findOne({
+    where: {
+      user_mail: req.body.user_mail,
+    },
+  })
+    .then(async(usuario) => {
+      //In case the email does not exist in the DB, the password is encrypted
+      if (!usuario) {
+        //let hash = await bcrypt.hash(req.body.password, 10);
+        const Newuser = user.build({
+          user_id: req.body.user_id,
+          user_name: req.body.user_name,
+          user_mail: req.body.user_mail,
+          user_password: req.body.user_password,
+          user_password_expired: req.body.user_password_expired,
+          user_branch_office_house_id: req.body.user_branch_office_house_id,
+          users_role_id: req.body.users_role_id,
+        });
+        Newuser.save();
+        res.send("Usuario guardado en la base de datos");
+      }
+      // If email exists in BD, please reply error message
+      else {
+        res.json({ error: "el usuario ya existe" });
+      }
+    })
+    .catch((err) => {
+      res.send("error:" + err);
+    });
+}
+
+
+//CONTROL IF THE EMAIL EXISTS IN THE DATABASE
+userCrtl.emailVerification= async(req,res) => {
+    const newuser = await user.findOne({
+        where: {
+            user_mail: req.body.user_mail
+        }
+    })
+
+    if(newuser){
+        console.log('already used');
+        console.log(newuser);
+        res.json({
+            'alreadyUsed' : true
+        })
+    }else
+    {
+        console.log('not used yet');
+        res.json({
+            'alreadyUsed' : false
+        })
+    }
+}
+
+
 
 //export module
 module.exports = userCrtl;
--- routes/users.js
+++ routes/users.js
@@ -5,60 +5,28 @@ const db = require("../db");
 const bcrypt = require('bcrypt');
 const router = express.Router();
 const {getUsers} =require('../controllers/usercontrollers')
+const {getUser} =require('../controllers/usercontrollers')
+const {saveUser} =require('../controllers/usercontrollers')
+const {emailVerification} =require('../controllers/usercontrollers')
 
-//Register
-router.post("/saveuser", (req, res) => {
-  //Check if the email exists in the DB
-  user.findOne({
-    where: {
-      mail: req.body.mail,
-    },
-  })
-    .then(async(usuario) => {
-      //In case the email does not exist in the DB, the password is encrypted
-      if (!usuario) {
-        let hash = await bcrypt.hash(req.body.password, 10);
-        const Newuser = user.build({
-          product: req.body.product,
-          mail: req.body.mail,
-          password: hash,
-          country: req.body.country,
-        });
-        Newuser.save();
-        res.send("Usuario guardado en la base de datos");
-      }
-      // If email exists in BD, please reply error message
-      else {
-        res.json({ error: "el usuario ya existe" });
-      }
-    })
-    .catch((err) => {
-      res.send("error:" + err);
-    });
-  });
-
-  //When you go to 'http://localhost:3000/api/users' you will get all the users stored in the database
-router.route('/todos')  
+//When you go to '/api/users/' you will get all the users stored in the database
+router.route('/')  
   .get(getUsers)
 
-//FOR DELETE AN USER 'http://localhost:3000/api/users/delete/:mail' FOR THE DATABASE
-/*router.get('/', async(req, res) => {
-    const users = await user.findAll();
-    res.send(users)
-})
-*/
-/*router.get("/",(req, res) =>{
-    User.findAll()
-    .then(users => {
-        res.send(users)
-    })
-    .catch(err => console.log(err));
-})*/
+//When you go to '/api/users/getuser' you will get one user by sending an user_mail
+router.route('/getuser/')  
+  .post(getUser)
 
+//For save one new user
+router.route('/saveuser/')  
+.post(saveUser)
+
+//POST FOR CONTROL IF THE EMAIL EXISTS IN THE DATABASE
+router.route('/emailverification/')  
+.post(emailVerification)
 
 //Login
 router.post("/login", (req, res) => {
-  //console.log(req.body);
   user.findOne({
     where: {
       mail: req.body.mail,
@@ -80,44 +48,6 @@ router.post("/login", (req, res) => {
     });
 });
 
-//POST FOR CONTROL IF THE EMAIL EXISTS IN THE DATABASE
-router.post('/emailverification', async(req, res) => {
-    const newuser = await user.findOne({
-        where: {
-            mail: req.body.mail
-        }
-    })
-
-    if(newuser){
-        console.log('already used');
-        console.log(newuser);
-        res.json({
-            'alreadyUsed' : true
-        })
-    }else
-    {
-        console.log('not used yet');
-        res.json({
-            'alreadyUsed' : false
-        })
-    }
-})
-
-//POST FOR FIND ONE USER WITH AN EMAIL
-router.post('/getUser', async(req, res) => {
-    const newuser = await user.findOne({
-        where: {
-            mail: req.body.mail
-        }
-    })
-    if(newuser){
-        console.log(req.session.user_id)
-        res.send(newuser)
-    }else
-    {
-        res.send(false)
-    }
-})
 
 //PUT Method for update the passwordExpired field of one user
 router.put('/updateUser/:mail', function (req, res) {
