ee0fd2a8fb756a55a037d88b117cc820c3a20824
alanfiz <alanfiz92@gmail.com>
2020-08-19 13:20:21 -0300

    getProduct ready. getProductsWFilters incomplete

--- controllers/productcontrollers.js
+++ controllers/productcontrollers.js
@@ -7,6 +7,38 @@ userCrtl.getProducts= async(req,res) => {
     res.json(products)
 }
 
+//GET all products with filters
+userCrtl.getProductsWFilters= async(req,res) => {
+    const products = await product.findAll({
+        where: {
+            product_name: req.body.product_name
+            // product_brand: req.body.product_brand,
+            // product_category: req.body.product_category,
+        }
+    });
+    if (products) {
+        res.json(products)
+    } else {
+        res.send(false)
+    }
+    
+}
+
+//GET one product with the id of product
+userCrtl.getProduct= async(req,res) => {
+    const products = await product.findOne({
+        where: {
+            product_id: req.body.product_id
+
+        }
+    });
+    if (products) {
+        res.json(products)
+    } else {
+        res.send(false)
+    }
+    
+}
 
 
 //export module
--- routes/products.js
+++ routes/products.js
@@ -3,12 +3,21 @@ const session = require("express-session");
 const db = require("../db");
 const bcrypt = require('bcrypt');
 const router = express.Router();
-const {getProducts} =require('../controllers/productcontrollers')
+const {getProducts, getProduct} =require('../controllers/productcontrollers')
+const {getProductsWFilters} =require('../controllers/productcontrollers')
 
 
-  //When you go to 'http://localhost:3000/api/products/' you will get all the users stored in the database
+  //When you go to 'http://localhost:3000/api/products/' you will get all the products stored in the database
 router.route('/')  
   .get(getProducts)
+
+  //When you go to 'http://localhost:3000/api/products/filters' you will get all the products with filters stored in the database
+  router.route('/filters')  
+  .get(getProductsWFilters)
+
+    //When you go to 'http://localhost:3000/api/products/getproduct' you will get the product with id stored in the database
+  router.route('/getproduct')  
+  .get(getProduct)
 
 
 module.exports = router;
